9d29a4cbd2595c5e647545c3b0dc680c5184219d
Willow <zarateangel945@gmail.com>
2025-01-12 01:18:34 -0500

    Sobrecarga

--- src/EjemploSobrecarga.java
+++ src/EjemploSobrecarga.java
@@ -1,2 +1,7 @@
 public class EjemploSobrecarga {
+
+    public static void main(String[] args) {
+        Calculadora cal = new Calculadora();
+        System.out.println("Los numero a sumar (int): " + cal.sumar(1, 1));
+    }
 }
