1a13cc9d954ed95065cdc7afefad10f26ed3e0b6
Willow <zarateangel945@gmail.com>
2025-01-12 00:45:48 -0500

    Polimorfismo_Ejemplo

---
+++ src/Calculadora.java
@@ -0,0 +1,12 @@
+
+// Sobre carga de datos - Polimorfismo
+public class Calculadora {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public float sumar(float a, float b) {
+        return a + b;
+    }
+}
